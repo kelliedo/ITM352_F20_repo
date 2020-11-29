47dee53da62af65d235294675bc60754f76c2c1a
kelliedo <70250940+kelliedo@users.noreply.github.com>
2020-11-28 15:04:53 -1000

    assignment2

--- Do_Kellie_Assignment2/server.js
+++ Do_Kellie_Assignment2/server.js
@@ -1,3 +1,4 @@
+// Kellie Do ITM 352 Assignment 2 
 // taken directly from Lab13 with modifications
 
 var express = require('express');
@@ -5,13 +6,28 @@ var app = express();
 var myParser = require("body-parser");
 var products = require("./products.json");
 var fs = require('fs');
-const queryString = require('query-string');
+var queryString = require('query-string');
+var data = require('./products.json');
+var filename = "user_data.json";
 
+app.use(express.static('./public'));
 
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
 });
+
+// taken from Lab 14, makes data readable or else outputs an error
+var filename = "user_data.json";
+
+if (fs.existsSync(filename)) {
+    data = fs.readFileSync(filename, 'utf-8');
+
+    user_data = JSON.parse(data);
+    console.log("User_data=", user_data);
+} else {
+    console.log("Sorry can't read file " + filename);
+    exit();
 
 app.use(myParser.urlencoded({ extended: true }));
 
@@ -44,80 +60,104 @@ function isNonNegInt(stringToCheck, returnErrors = false) {
 
     return returnErrors ? errors : (errors.length == 0);
 }
-
-// taken from Lab 14 with modifications
-
-var filename = "user_data.json";
-
-if (fs.existsSync(filename)) {
-    data = fs.readFileSync(filename, 'utf-8');
-
-    user_data = JSON.parse(data);
-    console.log("User_data=", user_data);
-} else {
-    console.log("Sorry can't read file " + filename);
-    exit();
 }
-
-app.get("/login", function (request, response) {
-    // Give a simple login form
-    str = `
-<body>
-<form action="/login" method="POST">
-<input type="text" name="username" size="40" placeholder="enter username" ><br />
-<input type="password" name="password" size="40" placeholder="enter password"><br />
-<input type="submit" value="Submit" id="submit">
-</form>
-</body>
-    `;
-    response.send(str);
-});
-
-app.post("/login", function (request, response) {
+// Lab 14 code; processes login form then goes to invoice
+app.post("/process_login", function (request, response) {
+    var login_error = [];
     console.log("Got a POST login request");
     POST = request.body;
-    user_name_from_form = POST["username"];
-    console.log("User name from form=" + user_name_from_form);
-    if (user_data[user_name_from_form] != undefined) {
-        response.send(`<H3> User ${POST["username"]} logged in`);
+    var stringified = queryString.stringify(POST);
+    form_username = POST["username"].toLowerCase();
+    console.log("Form username=" + form_username);
+    if (typeof user_data[form_username] != 'undefined') {
+      if (user_data[form_username].password == request.body.password) {
+        request.query.username = form_username;
+        console.log(user_data[request.query.username].name);
+        request.query.name = user_data[request.query.username].name
+        response.redirect('/invoice.html?' + stringified)
+        return;
+      }
+      else {
+        console.log(login_error);
+        request.query.username = form_username;
+        request.query.name = user_data[form_username].name;
+        request.query.login_error = login_error.join(';');
+      }
     } else {
-        response.send(`Error!`);
+      login_error.push = ('Invalid Username or Password');
+      console.log(login_error);
+      request.query.username = form_username;
+      request.query.name = user_data[form_username].name;
+      request.query.login_error = login_error.join(';');
     }
-});
-
-app.get("/register", function (request, response) {
-    // Give a simple register form
-    str = `
-<body>
-<form action="/register" method="POST">
-<input type="text" name="username" size="40" placeholder="enter username" ><br />
-<input type="password" name="password" size="40" placeholder="enter password"><br />
-<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
-<input type="email" name="email" size="40" placeholder="enter email"><br />
-<input type="submit" value="Submit" id="submit">
-</form>
-</body>
-    `;
-    response.send(str);
-});
-
-app.post("/register", function (request, response) {
-    // check registration info
-    POST = request.body;
+    response.redirect('./login_page.html?' + stringified);
+  });
+  // code to process registration
+  app.post("/process_register", function (request, response) {
+    let POST = request.body;
     console.log("Got register POST");
-    if (POST["username"] != undefined && POST['password'] != undefined) {  // make sure the password goes with the username
-        username = POST["username"];
-        user_data[username] = {};
-        user_data[username].name = username;
-        user_data[username].password = POST['password'];
-        user_data[username].email = POST['email'];
-
-        data = JSON.stringify(user_data);
-        fs.writeFileSync(filename, data, "utf-8");
-
-        response.send("User " + username + " logged in");
+    var register_errors = [];
+    var stringified = queryString.stringify(POST);
+  
+    // make sure username is between 4-10 characters and only consists of letters and numbers
+    if ((/.{3,9}/.test(POST['username'])) && (/^[a-zA-Z0-9]*$/.test(POST['username']))) {
+      console.log('username OK');
+    } else {
+      register_errors.push('Username must be a minimum of 4 characters, maximum of 10. Only letters and numbers allowed.');
     }
-});
+    // check that username is unique
+    var register_username = POST['username'].toLowerCase();
+    if (user_data[register_username] != 'undefined') {
+      register_errors.push('Username taken, please choose another one');
+    } else {
+      console.log('Valid username');
+    }
+  
+    // make sure password is more than 6 characters
+    if (POST['password'].length < 6) {
+      register_errors.push('Password length must be a minimum of 6 characters');
+    } else {
+      console.log('Valid password!');
+    }
+  
+    // check if both passwords match
+    if (POST['password'] == POST['confirmpassword']) {
+      console.log('passwords match');
+    } else {
+      register_errors.push('Please check that passwords match');
+    }
+  
+    // validate name
+    if (/^[A-Za-z]+$/.test(POST['name']) || (POST['name'] != "undefined")) {
+      console.log('full name OK');
+    } else {
+      register_errors.push('Letters only');
+    }
+    // validate email
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(POST['email'])) {
+      console.log('email OK');
+    } else {
+      register_errors.push('Invalid email');
+    }
+  
+    // if everything's OK then register user
+    if (register_errors.length == 0) { // no registration errors exist
+      // add registered user data to file
+      username = POST["username"];
+      user_data[username] = {};
+      user_data[username].name = username;
+      user_data[username].password = POST['password'];
+      user_data[username].email = POST['email'];
+  
+      data = JSON.stringify(user_data);
+      fs.writeFileSync(filename, data, "utf-8");
+  
+      response.redirect('./invoice.html?' + stringified);
+    } 
+    if (register_errors.length == 0) {
+      response.send("Sorry, try again");
+    }
+  });
 
 
 app.use(express.static('./public'));
